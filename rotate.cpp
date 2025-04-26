4e52129ed4b152b0fea0a017ab61ed89a5ae861d
KritiJadli <138752434+KritiJadli@users.noreply.github.com>
2025-04-26 22:17:10 +0530

    Add files via upload

---
+++ rotate.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+/*void rotate(int arr[], int n) {
+    int temp = arr[0];
+    for (int i = 1; i < n; i++) {
+        arr[i - 1] = arr[i]; // Shift elements to the left
+    }
+    arr[n - 1] = temp; // Put the first element at the end
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    rotate(arr, n); // Rotate array once to the left
+
+    cout << "Rotated array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}*/
+//remove duplicated number from sorted array
+
+
+
